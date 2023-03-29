3e4faa91a2792540df09d588920b7a142faa61ca
Naomi-Pate <patenaomi99@gmail.com>
2023-03-29 02:12:02 -0400

    Prepped html files, JS needs to be worked on and I may need a second js file

---
+++ script.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/";
+let userSearchTerm = document.querySelector("#searchbox").value;
+const searchCocktailNameAZ = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+const searchCocktailName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${}`;
