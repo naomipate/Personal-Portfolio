e046857897c46e41ed640ca43987512da07bd43c
Naomi-Pate <patenaomi99@gmail.com>
2023-03-29 13:06:22 -0400

    designed and added logo made on Canva

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/";
 let userSearchTerm = document.querySelector("#searchbox").value;
 const searchCocktailNameAZ = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
-const searchCocktailName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${}`;
+const searchCocktailName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSearchTerm}`;
